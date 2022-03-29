a26043f5579f004279a0768c79006d5c8824d195
HassanAhmed <drhassanahmed123@gmail.com>
2022-03-30 01:05:50 +0500

    javaScript 1 to 13

---
+++ app.js
@@ -0,0 +1,538 @@
+// //// Q#1 , Q#2 ,Q#3//////
+// alert ("Error! Please enter a valid password \n enter password again")
+// ///Q#4////
+// alert ("Welcome to JS land..")
+// alert ("Happy coading.! \n prevent this dialog to js land..")
+// /////Q#5///
+// alert ("Heloo...i can run js web page with console");
+// var Hello = "Heloo...i can run js web page with console"
+// console.log( Hello);
+
+
+// // //// chapter 2////
+
+// //////Q#1///
+// var username = "";
+// //Q#2
+// var name = "Hassan Ahmed";
+// //Q#3
+// var message = "Hello World"
+// alert (message)
+// //Q#4
+// var name = "jhon doe"
+// var age = "15 years old"
+// var degree = "Crtifies mobile app development"
+// alert (name)
+// alert (age)
+// alert (degree)
+
+// // Q#6
+// alert ("pizza \n  pizz \n piz \n pi")
+// // Q#7
+// var email = ""
+// email = "my email is drhassanahmed123@gmail.com"
+// alert (email)
+// Q#8
+// var java =" yah! i can write javascript in HTML <br>"
+// document.write(java)
+// //Q#9
+// var design ="“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”"
+// alert(design)
+// document.write(design + "<br>")
+
+
+// // chapter 3
+
+// // Q#2
+// var number = 15
+// alert ( "you are visited this page " + number + "times")
+
+// // Q#3
+// var birth = "My birth year is 2006"
+// var datatype = "data type of my declaired variable is number"
+// document.write(birth + "<br>" + datatype)
+
+// Q#4
+// var customer = prompt("whats your name" , "first name")
+// var order = prompt("what are u going to be order")
+// var num = prompt("total number of item")
+// var done = "my order is this"
+// document.write(customer +" "+ order + " "+num +" "+done )
+
+//Chapter # 5
+
+// // Q#1
+// var num1 = +prompt("write a number you want to add")
+// var num2 = +prompt("Write another numner")
+// var res = num1 + num2
+// document.write("sum of " + num1 + " and" + num2 + " is" + res)
+
+
+// // Q#2
+// var num1 = +prompt("write any number for multiplication")
+// var num2 = +prompt("another number u want to multiply with")
+// var result = num1 * num2
+// document.write("you multiply this number so anwere is " + num1 + " X" + num2 + " =" + result);
+
+// var num1 = prompt("write any number to subtract for")
+// var num2 = prompt("another number to show answere")
+// var result = num1 - num2
+// document.write("the answer is " + num1 + " -" + num2 + " =" + result )
+
+// var num1 = prompt("write any number for division")
+// var num2 = prompt("another number to divide than give answer")
+// var result = num1 / num2
+// document.write("The answer is" + num1 + " /" + num2 + " =" + result)
+
+// Q#3
+// var declaration = "Value after variable declaration is :?"
+// var num = 5
+// document.write(" <b> Initial Value : </b>" + num +"<br>" )
+// var num2 =  ++num
+// document.write(" <b> value after increament is : </b>" + num2 + "<br>" )
+// var num3 = num2 + 7
+// document.write( "<b> value after ADDITION is : </b>" + num3 + "<br>")
+// var num4 = --num3
+// document.write( "<b> value after Decrement is : </b>" + num4 + "<br>")
+// var num5 = num4 % 3
+// document.write( "<b> The reminder is : </b>" + num5 + "<br>")
+
+// Q#4
+// alert("One movie ticket is RS#600 Only")
+// var ticket = 600
+// var num1 = prompt("How many ticket u want to buy")
+// var result = num1 * ticket
+// document.write("total cost to buy " + num1 + " tickets" + " is " + result)
+
+// Q#5
+// var table = +prompt("write any table")
+// var num1 = table * 1
+// var num2 = table * 2
+// var num3 = table * 3
+// var num4 = table * 4
+// var num5 = table * 5
+// var num6 = table * 6
+// var num7 = table * 7
+// var num8 = table * 8
+// var num9 = table * 9
+// var num10 = table *10
+// document.write("<b> Table of </b>" + table + "<br>")
+// document.write(table + " x 1 = " + num1 + "<br>")
+// document.write(table + " x 2 = " + num2 + "<br>")
+// document.write(table + " x 3 = " + num3 + "<br>")
+// document.write(table + " x 4 = " + num4 + "<br>")
+// document.write(table + " x 5 = " + num5 + "<br>")
+// document.write(table + " x 6 = " + num6 + "<br>")
+// document.write(table + " x 7 = " + num7 + "<br>")
+// document.write(table + " x 8 = " + num8 + "<br>")
+// document.write(table + " x 9 = " + num9 + "<br>")
+// document.write(table + " x 10 = " + num10 + "<br>")
+
+// Q#6
+// var one = prompt("write temperature into celceus to convert into Ferhinhite")
+// var num = one
+// var two = one = (one * 9/5) + 32
+// document.write( num + "C is "+ two + "F")
+
+// var f = prompt("write temperature into Ferhenhite to convert into Celcius")
+// var num = f
+// var result = f = (f - 32) * 5/9
+// var result1 = result.toFixed(2)
+// document.write( num + "F is "+ result1 + "F")
+
+
+// Q#7
+// var num1 = 650
+// alert("price of 1 item is = " + num1)
+// var num2 = prompt("How much you Buy");
+// var result = num1 * num2
+// document.write("Total of Item is " + result + "</br>")
+
+// alert("price of item 2 is =" + 100)
+// var price = 100
+// var price1 = prompt("how much you buy");
+// var result1 = price * price1
+// document.write("Total of Item is RS " + result1 +"</br>")
+
+// var shipping = "shipping charges per delivery = 250each"
+// var shippingPrice = 250
+// document.write(shipping +"</br>")
+
+// var total = result + result1 + shippingPrice
+// document.write( "the Total price is RS " + total)
+
+// // Q#8
+// alert("See your Pecentage")
+// var obtaining = prompt("write your obtaining marks")
+// document.write("Marks Obtained: " + obtaining + "</br>")
+// var totalMarks = prompt("whats your total marks")
+// document.write("Total Marks: " + totalMarks + "</br>")
+
+// var result = obtaining * 100 / totalMarks + "%"
+// document.write("Percentage: " + result )
+
+// // Q#9
+// var user = prompt("how much USD you wanted to convert in PKR")
+// var num1 = user * 178
+// document.write("Total currency in PKR is " + num1 + "</br>" ) 
+
+// // Q#10
+// var user = prompt("how much RIYAl you wanted to convert in PKR")
+// document.write("you wanted to convert " + user + " RIYAl in PKR " + "</br>")
+// var num1 = user * 45
+// document.write("total currency in PKR is " + num1 + "</br>")
+
+// // Q#11
+// var year = prompt("Write Current YEAR?")
+// document.write("Current Year: " + year + "</br>")
+// var born = prompt("In which YEAR you born?")
+// document.write("Birth Year: " + born + "</br>")
+// var result = year - born
+// document.write("your Age is: " + result);
+
+
+// Q#12
+// var radius = prompt("write the Radius of Circle")
+// document.write("Value of Circle: " + radius + "</br>")
+// var pie = 3.142
+// var formulla = 2 * pie * radius
+// var formulla1 = formulla.toFixed(2)
+// document.write("The Circumfairence of Circle is: " + formulla1)
+
+// // Q#13
+// var snak = prompt("whats your favourit snack:")
+// document.write("Favourit snack: " + snak + "</br>")
+// var currentAge = prompt("whats your Current age: ")
+// document.write("Current Age: " + currentAge + "</br>")
+// var age = prompt("store a maximum age ")
+// document.write("Estimate maximum age: " + age + "</br>")
+// var amount = prompt("Store an estimated amount per day (as a number). ")
+// document.write("Amount of snacks per day is: " + amount + "</br>")
+
+
+///////////////chapter # 6///////////////////
+
+// Q#1
+// var num = prompt("Give any number")
+// document.write("Result: " + num + "</br>")
+// var value = "The value of a is: " + num 
+// document.write(value + "</br>" + "</br>")
+
+// var num1 = ++num
+// var result = "The value of ++a is: " + num1
+// document.write(result + "</br>")
+// var result = "Now the value of a is: " + num1 
+// document.write(result + "</br>" +  "</br>")
+
+// var num = num1
+// var result = "The valu of a++ is: " + num
+// document.write(result + "</br>" )
+// var result = num++ 
+// document.write( "Now the value of a is: " + num + "</br>" + "</br>")
+
+// var num = --num
+// var result = "The value of --a is: " + num
+// document.write(result + "</br>")
+// var result = "now the value of a is: " + num
+// document.write(result + "</br>" + "</br>")
+
+// var num = num--
+// var result = "the value of a-- is: " + num--
+// document.write(result + "</br>")
+// var result = "Now the value of a is " + num--
+// document.write(result); 
+
+// Q#2
+// var valueA = prompt("write any number")
+// var result1 = "The value of a is: " + valueA
+// document.write(result1+ "</br>")
+// var valueB = prompt("write any number")
+// var result2 = "The value of b is: " + valueB
+// document.write(result2+ "</br>" + "</br>")
+
+// var test = --valueA - --valueB + ++valueB + valueB--
+// document.write( "The answer is : " + test)
+
+
+// Q#3
+// alert("MARKSHEET")
+// var name = prompt("Write your name ")
+// var clas = prompt("Write your class , section also")
+// document.write("<b>" + name , clas + "</b>" + "</br>" )
+
+// var sub1 = +prompt("Write Obtain English Number")
+// document.write(" <b> English Marks:</b> " + sub1 + "</br>")
+
+
+// var sub2 = +prompt("Write Obtain Islamiat Number")
+// document.write(" <b> Islamiat Marks:</b> " + sub2 + "</br>")
+
+// var sub3 = +prompt("Write Obtain Biology Number")
+// document.write(" <b> Biology Marks:</b> " + sub3 + "</br>")
+
+
+// var total = 150
+// var add = sub1 + sub2 + sub3
+// var percent = add * 100 / total
+// var result = percent
+// document.write("PERCENTAGE : " + result)
+
+
+//////////////////////CHAPTER # 9/////////////////
+
+// Q#1
+// var city = prompt("city name")
+// if( city === "karachi"){
+//     alert("Welcome To Light Of City")
+// }
+// else{
+//     alert ("wrong city")
+// }
+
+// Q#2
+// var gender = prompt("write your gender here")
+// if(gender === "male"){
+//     alert("good morning sir")
+//     document.write("Gender: " + gender + "</br>")
+//     document.write("good morning sir")
+// } 
+// if(gender === "female"){
+//     alert("good morning maam")
+//     document.write("Gender: " + gender + "</br>")
+//     document.write("good morning maam")
+// }
+// else{
+//     alert("no responce")
+// }
+
+// // Q#3
+// var signal = prompt("what is the trafic singal color?")
+// document.write("Trafic Signal Color is :" + signal + "</br>")
+// var another = "go home and sleep"
+// if(signal === "red"){
+//     alert("Must Stop");
+//     document.write( "The Trafic Color say :  Must Stop !" + "</br>")
+
+// }
+// if(signal === "yellow"){
+//     alert("Ready to Move");
+//     document.write("The Trafic say : Ready to Move" + "</br>")
+// }
+// if(signal === "green"){
+//     alert("Move Now");
+//     document.write("The Trafic say :  Move Now" + "</br>")
+// }
+// else {
+//     alert(another);
+// }
+
+// // Q#4
+// var fuel = prompt("what is the remaing fuil in car?")
+// if(fuel <= 0.25 ){
+//     alert("please refuill the fuel in car ")
+// }  
+// else{
+//     alert("move a head")
+// }
+
+// Q#5
+// var a = 4;
+// if (++a === 5){
+// alert("given condition for variable a is true");
+// }
+
+// var b = 82;
+// if (b++ === 83){
+// alert("given condition for variable b is true");
+// }
+
+// var c = 12;
+// if (c++ === 13){
+// alert("condition 1 is true");
+// }
+// if (c === 13){
+// alert("condition 2 is true");
+// }
+// if (++c < 14){
+// alert("condition 3 is true");
+// }
+// if(c === 14){
+// alert("condition 4 is true");
+// // }
+
+// var materialCost = 20000;
+// var laborCost = 2000;
+// var totalCost = materialCost + laborCost;
+// if (totalCost === laborCost + materialCost){
+// alert("The cost equals");
+// }
+// if (true){
+//     alert("True");
+//     }
+//     if (false){
+//     alert("False");
+//     }
+
+// if("car" < "cat"){
+//     alert("car is smaller than cat");
+//     }
+
+
+// Q#6
+// var totalMarks = 300
+// document.write( "Total Marks : " + totalMarks  + "</br>")
+
+// var obt = +prompt("write the Marks you Obtain out of " + totalMarks)
+// document.write("Marks Obtain = " + obt  + "</br>")
+
+// var result = obt * 100 / totalMarks
+// document.write( "Percentage% = "+ result  + "</br>" )
+
+// if(result >= 80 || result < 100){
+//     document.write("Grade : A-one" + "</br>" + "Remarks : Excellent" + "</br>" )
+// }
+// if(result >= 70 || result < 80){
+//     document.write("Grade : A" + "</br>" + "Remarks : Good" + "</br>" )
+// }
+// if(result >= 60 || result < 70){
+//     document.write("Grade : Fail"  + "</br>" + "Remarks : Sorry"  + "</br>")
+// }
+
+
+// Q#7
+
+// var secret = 9
+// var secret2 = 10
+
+// var guess = +prompt("Guess any number 1 to 10")
+
+// if(guess === secret){
+//     document.write("Your Guess is correct HURRAH" + "</br>")
+// }
+// if(guess === secret2 ){
+//     document.write("“Close enough to the correct answer”.")
+// }
+// if (guess !== secret) {
+//     alert("Try again")
+// }
+
+
+
+
+// // Q#8
+// var user = +prompt("enter the number divisible by 3")
+// document.write("You write this  number : " + user + "</br>")
+
+// if ( user % 3 === 0){
+//     document.write("Your answer is Divisible by 3"  )
+// }
+// else{
+//     alert("your answer is not divisible by 3")
+// }
+
+// // Q#9
+// var user = +prompt("enter a number")
+// document.write("You write this = " + user + "</br>")
+
+// if(user % 2 === 0){
+//     document.write("You number is even")
+// }
+// else{
+//     alert("Your Number is Odd")
+// }
+
+// Q#10
+// var user = +prompt("What temperature today ?")
+// if(user > 40){
+//     alert("its too hot Outside")
+// }
+// if(user > 30){
+//     alert("“The Weather today is Normal.”")
+// }
+// if(user > 20){
+//     alert("“Today’s Weather is cool.”")
+// }
+// if(user > 10){
+//     alert("“OMG! Today’s weather is so Cool.”")
+// }
+
+
+// Q#11
+// var num1 = +prompt("First number")
+// var operator = +prompt("Write any operator + , - , * , % , /")
+// var num2 = +prompt("second number")
+
+// if(operator = "+"){
+//     alert(num1 + num2);
+// }
+
+//  if(operator = "-"){
+//     alert(num1 - num2);
+// }
+
+//  if(operator = "*"){
+//     alert(num1 * num2);
+// }
+
+//  if(operator = "%"){
+//     alert("your reminder is " + num1 % num2);
+// }
+
+//  if(operator = "/"){
+//     alert(num1 / num2);
+// }
+
+
+////////////////// CHAPTER # 12 / 13 ///////////////////
+
+// Q#5
+// var input = +prompt("write your password")
+// var input2 = +prompt("rewrite password again")
+// document.write("your password is " + input + "</br>")
+// document.write("Again " + input2 + "</br>")
+
+// if(input === input2){
+//     alert("you are login")
+//     document.write("You are log in")
+// }
+// else{
+//     alert("Password not same")
+//     document.write("RE-Try Please")
+// }
+
+// Q#6
+// var greeting;
+// greeting = hour
+// var hour = 13;
+// if (greeting < 18) {
+//     alert(greeting = "Good day");
+// }
+// else{
+// alert(greeting = "Good evening");
+// }
+
+// Q#7
+// var input = +prompt("What time is it in 24hours format?")
+// document.write("The Time is = " + input + "</br>")
+
+// var time = input
+// if(time >= 0000 && time <= 1200){
+//     alert("Good Morning")
+//     document.write("The Time Says = Good Morning " + "</br>")
+// }
+// if(time >= 1200 && time <=1700){
+//     alert("Good AfterNoon")
+//     document.write("The Time Says = Good AfterNoon" + "</br>")
+// }
+
+// if(time >= 1700 && time <=2100){
+//     alert("Good Evening")
+//     document.write("The Time Says = Good Evening" + "</br>")
+// }
+
+// if(time >= 2100 && time <=2359){
+//     alert("Good Night")
+//     document.write("The Time Says = Good Night" + "</br>")
+// }
+
+
